7afd022d79df5c104a5cbb09d2b74a2799303d10
beyondie2 <heewoong7843@gmail.com>
2024-10-16 22:44:31 +0900

    함수 require 관련 주석

--- temp.js
+++ temp.js
@@ -1,5 +1,6 @@
 // require 의 매개변수에 export할 파일 넣음
 const { odd, even } = require('./var');
+// 함수도 반환값의 형태로 require의 매개 변수에 넣을 수 있음
 const checkNumber = require('./func');
 
 function checkStringOddOrEven(str) {
